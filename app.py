58cd9b1d1f974604521efc7114c4e9c44e9c14f6
gilsaia <794433219@qq.com>
2019-05-18 15:29:30 +0800

    template

--- app.py
+++ app.py
@@ -25,35 +25,5 @@ def npr():
     return render_template("formversion/npr.html")
 
 
-@app.route('/contact')
-def contact():
-    return render_template("eVersion/Bcon.html")
-
-
-@app.route('/intro')
-def show_info():
-    return render_template("eVersion/Bintro.html")
-
-
-@app.route('/researchA')
-def show_pp():
-    return render_template("eVersion/Bpp.html")
-
-
-@app.route('/researchA2')
-def show_pp2():
-    return render_template("eVersion/Bpp2.html")
-
-
-@app.route('/researchA3')
-def show_pp3():
-    return render_template("eVersion/Bpp3.html")
-
-
-@app.route('/researchA4')
-def show_pp4():
-    return render_template("eVersion/Bpp4.html")
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

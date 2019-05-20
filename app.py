ab68fc55c7dbefb3390141aba8fcd52d3780a20c
xy <17822018007@163.com>
2019-05-20 18:49:12 +0800

    sdfsdf

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, g, redirect, url_for
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
 
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    return render_template("formversion/index.html")
+
+
+@app.route('/gallery')
+def gallery():
+    return render_template("formversion/gallery.html")
+
+
+@app.route('/generic')
+def generic():
+    return render_template("formversion/generic.html")
+
+
+@app.route('/npr',methods=['POST','GET'])
+def npr():
+    return render_template("formversion/npr.html")
+
+
+@app.route('/npr_api',methods=['POST'])
+def npr_api():
+    return render_template("formversion/gallery.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)

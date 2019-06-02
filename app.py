35c8ee5c7e78dff5fd7128c9aedcb19bf0d765e5
gilsaia <794433219@qq.com>
2019-06-02 14:21:49 +0800

    Merge branch 'master' of https://github.com/WArushrush/web_use
    # Conflicts:
    #	app.py
    #	templates/formversion/npr.html

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config.from_object(config)
 
 # 配置上传文件存放路径
 base = os.path.dirname(os.path.abspath(__file__))
-base = os.path.join(base, 'static')
+base = os.path.join(base, 'upload')
 app.config['UPLOADS_DEFAULT_DEST'] = base
 photo = UploadSet()
 configure_uploads(app, photo)
@@ -17,36 +17,36 @@ configure_uploads(app, photo)
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    return render_template("formversion/index.html")
+    return render_template("index.html")
 
 
 @app.route('/gallery')
 def gallery():
-    return render_template("formversion/gallery.html")
+    return render_template("gallery.html")
 
 
 @app.route('/generic')
 def generic():
-    return render_template("formversion/generic.html")
+    return render_template("generic.html")
 
 
 @app.route('/npr', methods=['POST', 'GET'])
 def npr():
     if request.method == "GET":
-        return render_template("formversion/npr.html")
+        return render_template("npr.html")
     else:
         # 接收图片
-        if(os.path.exists('static/files/img1/pic1.png')):
-            os.remove('static/files/img1/pic1.png')
+        if(os.path.exists('upload/images/pic1.png')):
+            os.remove('upload/images/pic1.png')
         random_num = str(random.randint(10, 10000000))
         random_num=random_num+".png"
         photo.save(request.files['file'], 'img1', random_num)  # 保存图片
         # print(request.data['hours']);
         return jsonify({
-            'status':'0',
-            'info':'new image',
-            'npr_img':"static/files/img1/"+random_num,
-            'input_img':"static/files/img1/"+random_num
+            'status': '0',
+            'info': 'new image',
+            'npr_img': 'upload/images/'+random_num,
+            'input_img': 'upload/images/'+random_num
         })
     # return render_template("formversion/npr.html")
 
@@ -54,7 +54,7 @@ def npr():
 @app.route('/npr_api',methods=['POST'])
 def npr_api():
     request.files
-    return render_template("formversion/gallery.html")
+    return render_template("gallery.html")
 
 
 if __name__ == '__main__':
